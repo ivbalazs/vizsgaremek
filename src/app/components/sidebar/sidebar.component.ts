02d6acf4c4bb21278e5a21ddc6ac0307bc709dc3
ivbalazs <ivbalazs@gmail.com>
2021-07-20 14:11:48 +0200

    dashboard component 1.0

--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -9,10 +9,10 @@ declare interface RouteInfo {
 }
 export const ROUTES: RouteInfo[] = [
   { path: '/dashboard', title: 'Dashboard', icon: 'leaderboard', class: '' },
+  { path: '/income', title: 'Bevételek', icon: 'account_balance_wallet', class: '' },
   { path: '/cost', title: 'Költségek', icon: 'credit_card', class: '' },
   { path: '/costcategory', title: 'Költség kategóriák', icon: 'category', class: '' },
   { path: '/costservice', title: 'Költség szolgáltatók', icon: 'store', class: '' },
-  { path: '/income', title: 'Bevételek', icon: 'account_balance_wallet', class: '' },
 ];
 
 @Component({
--- src/app/page/dashboard/dashboard.component.ts
+++ src/app/page/dashboard/dashboard.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { CostService } from 'app/service/cost.service';
+import { IncomeService } from 'app/service/income.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -7,9 +9,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
 
-  constructor() { }
+  sumOfCosts: number = 0;
+  sumOfIncomes: number = 0;
+  balance: number = 0;
+
+  constructor(
+    private costService: CostService,
+    private incomeService: IncomeService
+  ) { }
 
   ngOnInit(): void {
+    this.costService.list$.subscribe(
+      data => {
+        this.sumOfCosts = data.map(b => b.sum).reduce((prev, curr) => prev + curr, 0);
+      }
+
+    )
+
+    this.incomeService.list$.subscribe(
+      data => {
+        this.sumOfIncomes = data.map(b => b.sum).reduce((prev, curr) => prev + curr, 0);
+        this.balance = this.sumOfIncomes - this.sumOfCosts;
+      }
+
+    )
+
+    this.costService.getAll();
+    this.incomeService.getAll();
+
   }
 
+
+
 }
+
+
+
+
